1a126fb328dea44ea38eb121fe3422981adc160d
Wyatt323 <71224625+Wyatt323@users.noreply.github.com>
2022-12-19 23:22:24 +0800

    Update Debian11 root ssh.sh

--- Debian11 root ssh.sh
+++ Debian11 root ssh.sh
@@ -2,7 +2,7 @@
 
 # Check if script is being run as root
 if [ "$(id -u)" != "0" ]; then
-   echo "This script must be run as root" 1>&2
+        echo "This script must be run as root" 1>&2
    exit 1
 fi
 
@@ -14,6 +14,7 @@ fi
 
 # Enable root login
 sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
+
 sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/g' /etc/ssh/sshd_config
 
 # Restart SSH service
