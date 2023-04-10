4c5e18b59e4e2d51a1d9f61ad65eb92a7be6f398
Wyatt323 <354494308@qq.com>
2023-04-10 16:02:48 +0800

    1

--- remove-nginx.sh
+++ remove-nginx.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # 删除Nginx配置文件
-rm -rf /etc/nginx/
+sudo rm -rf /etc/nginx/
 
 # 删除Nginx安装文件
-apt-get purge nginx nginx-common nginx-full nginx-core
+sudo apt-get purge nginx nginx-common nginx-full nginx-core
 
 # 删除Nginx日志文件和缓存
-rm -rf /var/log/nginx/
-rm -rf /var/cache/nginx/
+sudo rm -rf /var/log/nginx/
+sudo rm -rf /var/cache/nginx/
 
 # 删除Nginx用户
-userdel -r nginx
+sudo userdel -r nginx
 
 echo "Nginx所有文件已被完全删除。"
+
